6e12158c27e439b5bcb510694efdd472b8de230b
AriSevcik <ari.sevcik.navarro@gmail.com>
2022-11-25 12:00:35 +0100

    Enemies

--- Game/Game/Source/Enemies.h
+++ Game/Game/Source/Enemies.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef __ENEMIES_H__
+#define __ENEMIES_H__
+
+#include "Entity.h"
+#include "Point.h" 
+#include "Animation.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class Enemy : public Entity
+{
+public:
+
+	Enemy();
+
+	virtual ~Enemy();
+
+	virtual bool Awake();
+
+	virtual bool Start();
+
+	virtual bool Update();
+
+	virtual bool CleanUp();
+
+	virtual void OnCollision(PhysBody* otherBody);
+
+public:
+
+private:
+
+
+};
+
+#endif //__ENEMIES_H__
