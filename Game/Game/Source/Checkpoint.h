774acf4827df3c4171ee8481edf3b2401ac478ec
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2023-01-25 18:25:34 +0100

    checkpoints and gitignore

---
+++ Game/Game/Source/Checkpoint.h
@@ -0,0 +1,43 @@
+#ifndef __CHECKPOINT_H__
+#define __CHECKPOINT_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class Checkpoint : public Entity
+{
+public:
+
+	Checkpoint();
+	virtual ~Checkpoint();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update();
+
+	bool CleanUp();
+
+	void OnCollision(PhysBody* otherBody);
+
+	bool CheckPickingCoin();
+
+public:
+
+	bool isPicked = false;
+
+private:
+
+	SDL_Texture* texture;
+	const char* texturePath;
+
+	//TODO 4: Add a physics to an item
+	PhysBody* pBody;
+
+};
+
+#endif // __CHECKPOINT_H__
