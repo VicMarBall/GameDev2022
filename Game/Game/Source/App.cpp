f7f0c5d3b4e51151761afa19851b21dfbc0c5e1f
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2023-01-28 20:18:44 +0100

    vsync true/false

--- Game/Game/Source/App.cpp
+++ Game/Game/Source/App.cpp
@@ -242,8 +242,8 @@ void App::FinishUpdate()
 	}
 
 	static char title[256];
-	sprintf_s(title, 256, "Av.FPS: %.2f Last sec frames: %i Last dt: %.3f Time since startup: %.3f Frame Count: %I64u ",
-		averageFps, framesPerSecond, dt, secondsSinceStartup, frameCount);
+	sprintf_s(title, 256, "Av.FPS: %.2f Last sec frames: %i Last dt: %.3f Time since startup: %.3f Frame Count: %I64u Vsync %i",
+		averageFps, framesPerSecond, dt, secondsSinceStartup, frameCount, vsync);
 
 	app->win->SetTitle(title);
 }
